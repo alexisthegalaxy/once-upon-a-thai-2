e0687ba428ce5a47c99c79233145905561f20bfd
Alexis Viand <alexis@memrise.com>
2020-12-06 02:35:22 +0100

    Improve Purifying test

--- Test/Purifying/PurifyingWordDock.gd
+++ Test/Purifying/PurifyingWordDock.gd
@@ -6,15 +6,17 @@ var word_id
 var index = 0
 var letters = []
 signal dock_is_selected
+signal erase_letters
 
 func _ready():
 	$Label.text = ""
+	$BackButton.hide()
 
 func update_color():
 	if is_selected:
-		modulate = Color(0.16, 1, 1, 1)
+		$Button.modulate = Color(0.16, 1, 1, 1)
 	else:
-		modulate = Color(1, 1, 1, 1)
+		$Button.modulate = Color(1, 1, 1, 1)
 
 func init_purifying_dock(_word_id, _index):
 	word_id = _word_id
@@ -35,4 +37,11 @@ func add_letter(letter_id):
 	letters.append(letter_id)
 	var letter = Game.letters[str(letter_id)]
 	$Label.text += letter.th.replace("-", "")
-	print('new label text ', $Label.text)
+	$BackButton.show()
+
+func _on_BackButton_pressed():
+	$Label.text = ""
+	emit_signal("erase_letters", index, letters)
+	letters = []
+	$BackButton.hide()
+
--- Test/Purifying/TestPurifying.gd
+++ Test/Purifying/TestPurifying.gd
@@ -15,7 +15,12 @@ func init_docks():
 	for word_id in word_ids:
 		docks[i].init_purifying_dock(word_id, i)
 		docks[i].connect("dock_is_selected", self, "selects_dock")
+		docks[i].connect("erase_letters", self, "dock_erase_letters")
 		i += 1
+
+func dock_erase_letters(dock_index, erased_letter_ids):
+	for letter_id in erased_letter_ids:
+		add_letter(letter_id)
 
 func selects_dock(dock_index):
 	selected_dock_index = dock_index
@@ -49,10 +54,13 @@ func init_letters():
 	for word_id in word_ids:
 		letter_ids += Game.words[str(word_id)].letters
 	for letter_id in letter_ids:
-		var purifying_test_letter = PurifyingTestLetter.instance()
-		purifying_test_letter.init_purifying_test_letter(letter_id)
-		purifying_test_letter.connect("purifying_letter_is_selected", self, "selects_letter")
-		self.add_child(purifying_test_letter)
+		add_letter(letter_id)
+
+func add_letter(letter_id):
+	var purifying_test_letter = PurifyingTestLetter.instance()
+	purifying_test_letter.init_purifying_test_letter(letter_id)
+	purifying_test_letter.connect("purifying_letter_is_selected", self, "selects_letter")
+	self.add_child(purifying_test_letter)
 
 func answered_correctly():
 	Game.active_test = null
