8a53d5a32300aff862aac8a8ed4db627f168cd48
Alexis Viand <alexis@memrise.com>
2020-12-27 00:02:13 +0100

    Minor improvements

--- UI/ExitAreYouSure.gd
+++ UI/ExitAreYouSure.gd
@@ -1,14 +1,9 @@
 extends CanvasLayer
 
-func stay():
-	Game.exit_screen = null
-	queue_free()
-
-func leave():
+func _on_Leave_pressed():
 	get_tree().quit()
 
-func _on_Leave_pressed():
-	leave()
-
 func _on_Continue_playing_pressed():
-	stay()
+	Game.exit_screen = null
+	Game.is_frozen = false
+	queue_free()
