bc9322629d36a491cc86d6d5172a9cee731f42be
Alexis Viand <alexis@memrise.com>
2021-04-09 22:56:31 +0100

    Add setting autotiles in Palace

--- Palace/EditorObject.gd
+++ Palace/EditorObject.gd
@@ -3,10 +3,27 @@ extends Node2D
 var can_place = true
 onready var level = get_node("/root/Palace/Level/YSort")
 var current_item
+var current_tileset = true
 
 func _process(delta):
 	global_position = get_global_mouse_position()
-	if current_item != null and can_place and Input.is_action_just_pressed("click"):
-		var new_item = current_item.instance()
-		level.add_child(new_item)
-		new_item.global_position = get_global_mouse_position() 
+	if can_place:
+		if current_item != null:
+			if Input.is_action_just_pressed("click"):
+				var new_item = current_item.instance()
+				level.add_child(new_item)
+				new_item.global_position = global_position
+		elif current_tileset:
+			var tilemap = get_node("/root/Palace/Level/CliffTileMap")
+			var tile_set = tilemap.tile_set
+			var tilemap_position = tilemap.world_to_map(global_position)
+			if Input.is_action_just_pressed("click"):
+				var tile_id = tile_set.find_tile_by_name("CliffTileset.png 1")
+				tilemap.set_cell(tilemap_position.x, tilemap_position.y, tile_id)
+			elif Input.is_action_just_pressed("right_click"):
+				var tile_id = -1  # We clear
+				tilemap.set_cell(tilemap_position.x, tilemap_position.y, tile_id)
+			var start = Vector2(tilemap_position.x - 1, tilemap_position.y - 1)
+			var end = Vector2(tilemap_position.x + 1, tilemap_position.y + 1)
+			tilemap.update_bitmask_region(start, end)
+
